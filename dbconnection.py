ecc8172c0f5c5cc3d171180d82ff4416454641e9
Antonio Netto <vasconcellosnetto@gmail.com>
2023-07-24 18:03:46 -0300

    Changes to main

--- dbconnection.py
+++ dbconnection.py
@@ -1,8 +1,8 @@
-import mysql.connector
-from mysql.connector import errorcode
 from mysql.connector import (connection)
+import settings
 
 class DBConnection():
+
     def __init__(self):
         self.conexao = connection.MySQLConnection(user='root', password='', host='127.0.0.1', database='cabeleleila_leila_db')
 
@@ -15,8 +15,7 @@ class DBConnection():
         resultado = c.fetchone()
 
         if resultado is not None:
-            print("true")
+            settings.usuario = resultado[2]
             return True
         else:
-            print("false")
             return False
---
+++ menu.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from dbconnection import DBConnection
+
+
+class Application:
+    db = DBConnection()
+    
+    def __init__(self, master=None):
+
+        self.fontePadrao = ("Arial", "10")
+        self.primeiroContainer = Frame(master)
+        self.primeiroContainer["pady"] = 10
+        self.primeiroContainer.pack()
+
+        self.segundoContainer = Frame(master)
+        self.segundoContainer["padx"] = 20
+        self.segundoContainer.pack()
+
+        self.terceiroContainer = Frame(master)
+        self.terceiroContainer["padx"] = 20
+        self.terceiroContainer.pack()
+
+        self.quartoContainer = Frame(master)
+        self.quartoContainer["pady"] = 20
+        self.quartoContainer.pack()
+
+        self.erroLabel = Label(self.primeiroContainer, text="", font = ("Arial", "10", "bold"), fg="red")
+        self.erroLabel.pack()
+
+        Label(self.primeiroContainer, text="Login no Sistema", font = ("Arial", "10", "bold")).pack()
+
+        Label(self.segundoContainer,text="Login: ", font=self.fontePadrao, width = 5).pack(side=LEFT)
+        self.usuario = Entry(self.segundoContainer, width = 20, font = self.fontePadrao)
+        self.usuario.pack(side=LEFT)
+
+        Label(self.terceiroContainer, text="Senha: ", font=self.fontePadrao, width = 5).pack(side=LEFT)
+        self.senha = Entry(self.terceiroContainer, width = 20, font = self.fontePadrao, show = "*")
+        self.senha.pack(side=LEFT)
+
+        Button(self.quartoContainer, text = "Autenticar", font = ("Arial", "8"), width = 12, command = self.verificaSenha).pack(side=LEFT)
+        Button(self.quartoContainer, text="Fechar", font = ("Arial", "8"), width = 12, command = root.destroy).pack(side=LEFT)
+
+    #Método verificar senha
+    def verificaSenha(self):
+        usuario = self.db.teste_login(self.usuario.get(), self.senha.get())
+        if self.db.teste_login(self.usuario.get(), self.senha.get()) is not None:
+            print(usuario)
+            root.destroy()
+        else:
+            self.erroLabel["text"] = "Usuário/senha incorreto!"
+
+root = Tk()
+root.eval('tk::PlaceWindow . center')
+root.title("Cabeleleila Leila")
+Application(root)
+root.mainloop()
