96f6ae4c5439fc8cd95deb9228e054e2e1a04978
Antonio Netto <vasconcellosnetto@gmail.com>
2023-07-25 23:04:36 -0300

    Update to the files

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Para executar o programa, é necessário:
     Instalar a biblioteca tkcalendar (pip install tkcalendar)
     Instalar a biblioteca tktimepicker (pip install tktimepicker)
     Executar o programa a partir do arquivo main.py
+
+Observações:
+    Alguns itens não foram resolvidos:
+        itens opcionais
+        agendamentos somente a partir da data/hora atual
+        janelas de cadastro e atualização não se fecham após apertar botão
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from tkinter import *
-from tkinter import ttk
-from tkcalendar import Calendar, DateEntry
+from tkinter import ttk, messagebox
+from tkcalendar import DateEntry
 from tktimepicker import SpinTimePickerOld, constants
 from dbconnection import DBConnection
 
-class Application:
-    db = DBConnection()
+class Application:    
 
     def __init__(self, master=None):
         self.fontePadrao = ("Arial", "10")
@@ -25,9 +24,6 @@ class Application:
         self.quartoContainer["pady"] = 20
         self.quartoContainer.pack()
 
-        self.erroLabel = Label(self.primeiroContainer, text="", font = ("Arial", "10", "bold"), fg="red")
-        self.erroLabel.pack()
-
         Label(self.primeiroContainer, text="Login no Sistema", font = ("Arial", "10", "bold")).pack()
 
         Label(self.segundoContainer,text="Login: ", font=self.fontePadrao, width = 5).pack(side=LEFT)
@@ -40,15 +36,15 @@ class Application:
 
         Button(self.quartoContainer, text = "Autenticar", font = ("Arial", "8"), width = 12, command = self.verificarLogin).pack(side=LEFT)
         Button(self.quartoContainer, text="Fechar", font = ("Arial", "8"), width = 12, command = root.destroy).pack(side=LEFT)
+    
+    db = DBConnection()
 
-    #Método verificar senha
     def verificarLogin(self):
         if self.db.testeLogin(self.login.get(), self.senha.get()):  
             self.telaInicial()
         else:
-            self.erroLabel["text"] = "Usuário/senha incorreto!"
+            messagebox.showerror("Erro!","Usuário/senha incorreto!")
 
-    #Construir menu
     def telaInicial(self):
         self.primeiroContainer.destroy()
         self.segundoContainer.destroy()
@@ -61,8 +57,8 @@ class Application:
 
         agendamento_opcao = Menu(barra_menu, tearoff=0)            
         barra_menu.add_cascade(label="Agendamento", menu=agendamento_opcao)
-        agendamento_opcao.add_command(label="Novo agendamento", command=self.novo_agendamento)
-        agendamento_opcao.add_command(label="Alterar agendamento", command="x = 0")
+        agendamento_opcao.add_command(label="Novo agendamento", command=self.novoAgendamento)
+        agendamento_opcao.add_command(label="Alterar agendamento", command=self.alterarAgendamento)
         agendamento_opcao.add_command(label="Histórico de agendamentos", command="x = 0")
 
         operacao_opcao = Menu(barra_menu, tearoff=0)            
@@ -77,10 +73,9 @@ class Application:
             
         barra_menu.add_command(label="Sair", command=root.quit)
             
-        root.config(menu=barra_menu)
+        root.config(menu=barra_menu)        
 
-    #Construir novo agendamento
-    def novo_agendamento(self):
+    def novoAgendamento(self):
         self.fonte = ("Verdana", "8")
 
         novoAgendamento = Toplevel(root)
@@ -129,7 +124,12 @@ class Application:
 
         Label(self.container3, text="Cabelereiro(a): ", font = ("Arial", "10", "bold")).pack(side=LEFT)
         self.funcionarios_cbb = ttk.Combobox(self.container3, textvariable=StringVar(), width = 50)
-        self.funcionarios_cbb['values'] = self.db.listarFuncionarios()
+
+        funcionarios = []
+        valores = self.db.listarFuncionarios()
+        for x in range(len(valores)):
+            funcionarios.append(valores[x][2])
+        self.funcionarios_cbb['values'] = funcionarios
         self.funcionarios_cbb.pack(side=LEFT)
 
         Label(self.container4, text="Procedimentos: ", font = ("Arial", "10", "bold")).pack(side=LEFT)
@@ -137,7 +137,7 @@ class Application:
         self.txt_procedimentos.pack(side=LEFT)
 
         Label(self.container5, text="Data: ", font = ("Arial", "10", "bold")).pack(side=LEFT)
-        self.data_cbb = DateEntry(self.container5, textvariable=StringVar(), width = 50)
+        self.data_cbb = DateEntry(self.container5, textvariable=StringVar(), width = 50, date_pattern="yyyy-MM-dd")
         self.data_cbb.pack(side=LEFT)
 
         Label(self.container6, text="Horário: ", font = ("Arial", "10", "bold")).pack(side=LEFT)
@@ -145,8 +145,105 @@ class Application:
         self.hora.addAll(constants.HOURS24)
         self.hora.pack(expand=True, fill="both", side=LEFT)
 
-        Button(self.container7, text = "Agendar", font = ("Arial", "8"), width = 12, command = self.verificarLogin).pack(side=LEFT)
-        Button(self.container7, text="Cancelar", font = ("Arial", "8"), width = 12, command = root.destroy).pack(side=LEFT)
+        Button(self.container7, text = "Agendar", font = ("Arial", "8"), width = 12, command = self.cadastrarAgendamento).pack(side=LEFT)
+        Button(self.container7, text="Cancelar", font = ("Arial", "8"), width = 12, command = novoAgendamento.destroy).pack(side=LEFT)
+
+    def alterarAgendamento(self):
+        self.fonte = ("Verdana", "8")
+
+        alterarAgendamento = Toplevel(root)
+        self.container1 = Frame(alterarAgendamento)
+        self.container1["pady"] = 20
+        self.container1.pack()
+
+        self.container2 = Frame(alterarAgendamento)
+        self.container2["padx"] = 20
+        self.container2["pady"] = 5
+        self.container2.pack()
+
+        self.container3 = Frame(alterarAgendamento)
+        self.container3["padx"] = 20
+        self.container3["pady"] = 5
+        self.container3.pack()
+
+        self.container4 = Frame(alterarAgendamento)
+        self.container4["padx"] = 20
+        self.container4["pady"] = 5
+        self.container4.pack()
+
+        self.container5 = Frame(alterarAgendamento)
+        self.container5["padx"] = 20
+        self.container5["pady"] = 5
+        self.container5.pack()
+
+        self.container7 = Frame(alterarAgendamento)
+        self.container7["pady"] = 15
+        self.container7.pack()
+
+        Label(self.container1, text="Alterar agendamento:", font = ("Arial", "10", "bold")).pack()
+
+        Label(self.container2, text="Cliente: ", font = ("Arial", "10", "bold")).pack(side=LEFT)
+        self.txt_cliente_login = Entry(self.container2, width = 50, font = self.fonte)
+        self.txt_cliente_login.pack(side=LEFT)
+        self.txt_cliente_login.insert(INSERT, self.db.verificarClientes())
+        self.txt_cliente_login.config(state=DISABLED)        
+
+        Label(self.container3, text="Data: ", font = ("Arial", "10", "bold")).pack(side=LEFT)
+        self.data_cbb = ttk.Combobox(self.container3, textvariable=StringVar(), width = 50)
+        datas = []
+        valores = self.db.listarAgendamentosUpdate()
+        for x in range(len(valores)):
+            datas.append(valores[x][0])
+        self.data_cbb['values'] = datas
+        self.data_cbb.pack(side=LEFT)
+        self.data_cbb.bind("<<ComboboxSelected>>", lambda _ : self.buscarAgendamento())
+
+        Label(self.container4, text="Cabelereiro(a): ", font = ("Arial", "10", "bold")).pack(side=LEFT)
+        self.funcionarios_cbb = ttk.Combobox(self.container4, textvariable=StringVar(), width = 50)
+
+        funcionarios = []
+        valores = self.db.listarFuncionarios()
+        for x in range(len(valores)):
+            funcionarios.append(valores[x][2])
+        self.funcionarios_cbb['values'] = funcionarios
+        self.funcionarios_cbb.pack(side=LEFT)
+
+        Label(self.container5, text="Procedimentos: ", font = ("Arial", "10", "bold")).pack(side=LEFT)
+        self.txt_procedimentos = Entry(self.container5, width = 50, font = self.fonte)
+        self.txt_procedimentos.pack(side=LEFT)
+
+        Button(self.container7, text = "Atualizar", font = ("Arial", "8"), width = 12, command = self.atualizarAgendamento).pack(side=LEFT)
+        Button(self.container7, text="Cancelar", font = ("Arial", "8"), width = 12, command = alterarAgendamento.destroy).pack(side=LEFT)
+
+    def cadastrarAgendamento(self):
+        hora = str(self.hora.hours24()) + ":" + str(self.hora.minutes())
+        try:
+            if self.db.cadastrarAgendamento(self.funcionarios_cbb.get(), self.data_cbb.get(), hora, self.txt_procedimentos.get()):  
+                messagebox.showinfo("Sucesso!","Agendamento feito com sucesso!")      
+            else:
+                messagebox.showerror("Erro!","Erro ao agendar!")
+        except:
+            messagebox.showerror("Erro!","Erro ao agendar!")
+
+    def atualizarAgendamento(self):
+        try:
+            if self.db.atualizarAgendamento(self.data_cbb.get(), self.txt_procedimentos.get()):  
+                messagebox.showinfo("Sucesso!","Atualização feita com sucesso!") 
+            else:
+                messagebox.showerror("Erro!","Erro ao atualizar!")
+        except:
+            messagebox.showerror("Erro!","Erro ao atualizar!")
+    
+    def buscarAgendamento(self):
+        info = self.db.listarAgendamentosData(self.data_cbb.get())
+        
+        self.funcionarios_cbb.config(state=NORMAL)
+        self.funcionarios_cbb.delete(0, END)
+        self.funcionarios_cbb.set(info[0][3])
+        self.funcionarios_cbb.config(state=DISABLED) 
+
+        self.txt_procedimentos.delete(0, END)
+        self.txt_procedimentos.insert(INSERT, info[0][1]) 
 
 root = Tk()
 root.eval('tk::PlaceWindow . center')
