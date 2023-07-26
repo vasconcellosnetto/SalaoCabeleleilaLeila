3ff57f36c98e25105841c22ec399c95ddf31c46e
Antonio Netto <vasconcellosnetto@gmail.com>
2023-07-25 23:04:24 -0300

    Updates to the files

--- dbconnection.py
+++ dbconnection.py
@@ -9,7 +9,7 @@ class DBConnection():
 
     def testeLogin(self, login, senha):
         c = self.conexao.cursor()
-        c.execute(f"SELECT * FROM( SELECT cliente_login AS login, cliente_nome AS nome, cliente_senha AS senha FROM clientes UNION ALL SELECT funcionario_login AS login, funcionario_nome AS nome, funcionario_senha AS senha FROM funcionarios) AS users WHERE login = '{login}' AND senha = '{senha}';")
+        c.execute(f"SELECT * FROM (SELECT cliente_login AS login, cliente_nome AS nome, cliente_senha AS senha FROM clientes UNION ALL SELECT funcionario_login AS login, funcionario_nome AS nome, funcionario_senha AS senha FROM funcionarios) AS users WHERE login = '{login}' AND senha = '{senha}'")
         resultado = c.fetchone()
         c.close()
         if resultado is not None:
@@ -20,7 +20,7 @@ class DBConnection():
         
     def listarFuncionarios(self):
         c = self.conexao.cursor()
-        c.execute("SELECT funcionario_nome FROM funcionarios")
+        c.execute("SELECT * FROM funcionarios")
         resultado = c.fetchall()
         c.close()
         return resultado
@@ -31,4 +31,41 @@ class DBConnection():
         resultado = c.fetchone() 
         c.close() 
         return resultado[0]
-
+    
+    def cadastrarAgendamento(self, funcionario, data, hora, procedimentos):
+        c = self.conexao.cursor()
+        c.execute(f"SELECT funcionario_login FROM funcionarios WHERE funcionario_nome = '{funcionario}'")
+        resultado = c.fetchone()
+        data_hora = str(data) + "T" + str(hora)
+        c.execute(f"INSERT INTO agendamentos (cliente_login, funcionario_login, agendamento_data, agendamento_servicos) VALUES ('{settings.usuario}', '{resultado[0]}', '{data_hora}', '{procedimentos}')")
+        self.conexao.commit()
+        c.close()
+        return True
+    
+    def atualizarAgendamento(self, data, procedimentos):
+        c = self.conexao.cursor()
+        c.execute(f"UPDATE agendamentos SET agendamento_servicos = '{procedimentos}' WHERE agendamentos.cliente_login = '{settings.usuario}' AND agendamento_data = '{data}'")
+        self.conexao.commit()
+        c.close()
+        return True
+    
+    def listarAgendamentosAll(self):
+        c = self.conexao.cursor()
+        c.execute(f"SELECT agendamento_data, agendamento_servicos, cliente_nome, funcionario_nome FROM agendamentos JOIN clientes ON (clientes.cliente_login = agendamentos.cliente_login) JOIN funcionarios ON (funcionarios.funcionario_login = agendamentos.funcionario_login) WHERE agendamentos.cliente_login = '{settings.usuario}'")
+        resultado = c.fetchall() 
+        c.close() 
+        return resultado
+    
+    def listarAgendamentosData(self, data):
+        c = self.conexao.cursor()
+        c.execute(f"SELECT agendamento_data, agendamento_servicos, cliente_nome, funcionario_nome FROM agendamentos JOIN clientes ON (clientes.cliente_login = agendamentos.cliente_login) JOIN funcionarios ON (funcionarios.funcionario_login = agendamentos.funcionario_login) WHERE agendamentos.cliente_login = '{settings.usuario}' AND agendamento_data = '{data}'")
+        resultado = c.fetchall() 
+        c.close() 
+        return resultado
+    
+    def listarAgendamentosUpdate(self):
+        c = self.conexao.cursor()
+        c.execute(f"SELECT agendamento_data, agendamento_servicos, cliente_nome, funcionario_nome FROM agendamentos JOIN clientes ON (clientes.cliente_login = agendamentos.cliente_login) JOIN funcionarios ON (funcionarios.funcionario_login = agendamentos.funcionario_login) WHERE agendamentos.cliente_login = '{settings.usuario}' AND DATE(agendamento_data) > DATE_ADD(DATE(NOW()), INTERVAL 2 DAY)")
+        resultado = c.fetchall() 
+        c.close() 
+        return resultado
